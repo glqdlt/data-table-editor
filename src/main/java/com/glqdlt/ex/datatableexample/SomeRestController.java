a2a4a2bc1c7e5519900b6dafe968ebb04303b241
iw.jhun <dlfdnd0725@gmail.com>
2018-06-21 23:13:18 +0900

    custom search 구현

--- src/main/java/com/glqdlt/ex/datatableexample/SomeRestController.java
+++ src/main/java/com/glqdlt/ex/datatableexample/SomeRestController.java
@@ -3,6 +3,7 @@ package com.glqdlt.ex.datatableexample;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,10 +14,15 @@ public class SomeRestController {
     @Autowired
     SomeService service;
 
-    @GetMapping("/api/user/find/all")
-    public DataTableVo<User> findUsers() {
+    @GetMapping("/api/user/find")
+    public DataTableVo<User> findUsers(@RequestParam(name = "searchType") Integer searchType, @RequestParam(name = "keyWord") String keyWord) {
+        log.info("type : {}, keyword : {}", searchType, keyWord);
+        if(searchType == 1 && !keyWord.equals("")){
+            List<User> user = service.findUser(keyWord);
+            return new DataTableVo<>(user, 1, user.size(), user.size());
+        }
         List<User> users = service.findUsers();
-        return new DataTableVo<>(users, 1, users.size(),users.size());
+        return new DataTableVo<>(users, 1, users.size(), users.size());
     }
 
 }
--- src/main/java/com/glqdlt/ex/datatableexample/SomeService.java
+++ src/main/java/com/glqdlt/ex/datatableexample/SomeService.java
@@ -10,14 +10,24 @@ import java.util.stream.IntStream;
 @Slf4j
 @Service
 public class SomeService {
-    public List<User> findUsers() {
-        List<User> users = IntStream.range(1, 100).boxed().map(i -> {
+    private final List<User> data;
+
+    public SomeService() {
+        this.data = IntStream.range(1, 100).boxed().map(i -> {
             User.UserSex someSex = User.UserSex.Woman;
             if (i % 2 == 0) {
                 someSex = User.UserSex.Man;
             }
             return new User(i, "some-user-" + i, someSex);
         }).collect(Collectors.toList());
+    }
+
+    public List<User> findUsers() {
+        List<User> users = data;
         return users;
+    }
+
+    public List<User> findUser(String keyWord) {
+        return data.stream().filter(x -> x.getName().equals(keyWord)).collect(Collectors.toList());
     }
 }
